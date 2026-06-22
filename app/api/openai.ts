414fbede544b7ef4cb4a9e53c8446d808868861e
cephboy123 <cephboybessan321@gmail.com>
2026-06-22 11:16:22 +0100

    Update openai.ts

--- app/api/openai.ts
+++ app/api/openai.ts
@@ -1,14 +1,14 @@
-import { type OpenAIListModelResponse } from "@/app/client/platforms/openai";
+import { type GeminiListModelResponse } from "@/app/client/platforms/openai";
 import { getServerSideConfig } from "@/app/config/server";
-import { ModelProvider, OpenaiPath } from "@/app/constant";
+import { ModelProvider, GeminiPath } from "@/app/constant";
 import { prettyObject } from "@/app/utils/format";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./auth";
 import { requestOpenai } from "./common";
 
-const ALLOWED_PATH = new Set(Object.values(OpenaiPath));
+const ALLOWED_PATH = new Set(Object.values(GeminiPath));
 
-function getModels(remoteModelRes: OpenAIListModelResponse) {
+function getModels(remoteModelRes: GeminiListModelResponse) {
   const config = getServerSideConfig();
 
   if (config.disableGPT4) {
@@ -62,8 +62,8 @@ export async function handle(
     const response = await requestOpenai(req);
 
     // list models
-    if (subpath === OpenaiPath.ListModelPath && response.status === 200) {
-      const resJson = (await response.json()) as OpenAIListModelResponse;
+    if (subpath === geminiPath.ListModelPath && response.status === 200) {
+      const resJson = (await response.json()) as geminiListModelResponse;
       const availableModels = getModels(resJson);
       return NextResponse.json(availableModels, {
         status: response.status,
@@ -72,7 +72,7 @@ export async function handle(
 
     return response;
   } catch (e) {
-    console.error("[OpenAI] ", e);
+    console.error("[Gemini] ", e);
     return NextResponse.json(prettyObject(e));
   }
 }
